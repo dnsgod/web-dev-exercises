ca9c8a94091876fb63b1399091e3df82fa86a937
Hohyeon <hoove@naver.com>
2025-01-06 10:38:13 +0900

    Add new file

---
+++ day05-javascript-array/Javascript-array-basic.js
@@ -0,0 +1,7 @@
+const fruits = ["apple", "banana", "cherry"];
+
+console.log(fruits[0]);
+fruits.push("orange");
+console.log(fruits);
+fruits.pop();
+console.log(fruits);
---
+++ day05-javascript-array/Javascript-array.js
@@ -0,0 +1,13 @@
+const student = {
+    name: "Jhon Doe",
+    age: 20,
+    grade: "A",
+};
+
+console.log(student);
+
+student.grade = "B";
+console.log(student.grade);
+
+student.major = "Computer Science";
+console.log(student);
